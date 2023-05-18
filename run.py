ed8695ebf2e599f6cf53295893f6bf5e831ccd8a
Anna Konstantynova <hannakonstantinova@gmail.com>
2023-05-18 08:57:16 +0000

    Create get_review function to get data string from user

--- run.py
+++ run.py
@@ -18,5 +18,25 @@ improve = SHEET_R.worksheet('improve')
 
 data = review.get_all_values()
 
-print(data)
 
+def get_review():
+    """
+    Get user's review asking three questions.
+    Get a point from 1 to 5 for each question.
+    1 is bad, 5 is good.
+    """
+    # second screen
+    print("Please answer three questions below:")
+    print("For each question give a point from 1 to 5, where 1 is bad and 5 is good")
+
+    data_str_food = input("How tasty was the food?\n")
+    data_str_service = input("How friendly was our staff?\n")
+    data_str_clean = input("How clean is our cafe?\n")
+    data_str_vibe = input("How do you like the atmosphere?\n")
+    print(f"Your review is {data_str_food}")
+    
+
+# first screen
+print("Thank you for visiting cafe 'Ciao'")
+print("Please take a few moments to leave us your review. It will help us to improve our service.")
+get_review()
